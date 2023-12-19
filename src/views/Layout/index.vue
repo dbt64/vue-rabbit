afbff020e08099b3f33aff1110e165e892009bac
zry <2942972964@qq.com>
2023-12-19 21:44:42 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,12 @@
-<template>
-  <div>我是首页</div>
-  <!-- 二级路由出口 -->
-  <RouterView />
-</template>
-
-<script>
-export default {};
+<script setup>
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
 </script>
 
-<style></style>
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
