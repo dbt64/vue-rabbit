76f8ff6a6acdb60a9e5b9402ea28739bf2890614
zry <2942972964@qq.com>
2023-12-27 15:41:25 +0800

    放大镜-实现大图显示以及优化

--- src/components/imagesView/index.vue
+++ src/components/imagesView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from "vue";
+import { ref, vShow, watch } from "vue";
+import { useMouseInElement } from "@vueuse/core";
 const props = defineProps({
   imageList: {
     type: Array,
@@ -11,6 +12,38 @@ const activeIndex = ref(0);
 const enterhandler = (index) => {
   activeIndex.value = index;
 };
+
+// 获取鼠标相对位置
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+const left = ref(0);
+const top = ref(0);
+
+const positionX = ref(0);
+const positionY = ref(0);
+
+watch([elementX, elementY, isOutside], () => {
+  if (isOutside.value) return;
+  // 有效范围内控制滑块距离
+  // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100;
+  }
+
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100;
+  }
+
+  if (elementX.value > 300) left.value = 200;
+  if (elementX.value < 100) left.value = 0;
+
+  if (elementY.value > 300) top.value = 200;
+  if (elementY.value < 100) top.value = 0;
+
+  // 控制大图的显示
+  positionX.value = -left.value * 2;
+  positionY.value = -top.value * 2;
+});
 </script>
 
 <template>
@@ -19,7 +52,11 @@ const enterhandler = (index) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div
+        class="layer"
+        v-show="!isOutside"
+        :style="{ left: `${left}px`, top: `${top}px` }"
+      ></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -37,12 +74,12 @@ const enterhandler = (index) => {
       class="large"
       :style="[
         {
-          backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`,
+          backgroundImage: `url(${imageList[activeIndex]})`,
+          backgroundPositionX: `${positionX}px`,
+          backgroundPositionY: `${positionY}px`,
         },
       ]"
-      v-show="false"
+      v-show="!isOutside"
     ></div>
   </div>
 </template>
