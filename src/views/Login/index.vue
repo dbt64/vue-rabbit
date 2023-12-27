892398e84d76cee43de059c18aa6eddeb5cc4841
zry <2942972964@qq.com>
2023-12-27 16:28:23 +0800

    表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,20 @@
-<script setup></script>
+<script setup>
+import { reactive, ref } from "vue";
+
+const form = reactive({
+  account: "",
+  password: "",
+});
+
+// 准备规则对象
+const rules = {
+  account: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+  password: [
+    { required: true, message: "密码不能为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码长度为6-14个字符", trigger: "blur" },
+  ],
+};
+</script>
 
 <template>
   <div>
@@ -21,12 +37,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form
+              :rules="rules"
+              :model="form"
+              label-position="right"
+              label-width="60px"
+              status-icon
+            >
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
