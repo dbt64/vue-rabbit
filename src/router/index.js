da05fec605c12691678e2bfb04a416609ca3dd23
zry <2942972964@qq.com>
2024-01-01 20:57:05 +0800

    切换地址-激活交互实现

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
+import Checkout from "@/views/Checkout/index.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -34,6 +36,10 @@ const router = createRouter({
         {
           path: "cartlist",
           component: CartList,
+        },
+        {
+          path: "checkout",
+          component: Checkout,
         },
       ],
     },
