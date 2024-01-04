cb31b7be96bd852b9b4cb4600d30c0905a1dda9e
zry <2942972964@qq.com>
2024-01-04 18:45:33 +0800

    订单-使用tab切换获取状态数据

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,9 @@ import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
+import Member from "@/views/Member/index.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -45,6 +48,20 @@ const router = createRouter({
         {
           path: "pay",
           component: Pay,
+        },
+        {
+          path: "member",
+          component: Member,
+          children: [
+            {
+              path: "user",
+              component: UserInfo,
+            },
+            {
+              path: "order",
+              component: UserOrder,
+            },
+          ],
         },
       ],
     },
